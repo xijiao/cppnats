8b6e6017edaa435723cc3f72618058bf93091561
xijiao <xjiaosdq@gmail.com>
2018-12-13 18:48:53 +0800

    fix compiling error on linux with libstdc++-4.8.5

--- include/cppnats.hpp
+++ include/cppnats.hpp
@@ -15,6 +15,17 @@
 
 #include <nats.h>
 
+#ifdef __GLIBCXX__
+#if __GLIBCXX__ <= 20150623
+namespace std { // back compatible for glibc++ < 4.9
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}; // end namespace std
+#endif // __GLIBCXX__ < 20150623
+#endif // __GLIBCXX__
+
 namespace cppnats {
 
 /** \brief Status represents the result of a NATS operation.
